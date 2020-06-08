386b4b2a50d49931d2e0edac32169944264d8642
LutfiArdiansyah <lutfi_ardiansyah@ymail.com>
2020-06-08 18:11:23 +0700

    Rewrite path

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+var allRequest = function (req, res, next) {
+  req.path.replace("cron-covid19", "");
+};
+
+app.use(allRequest);
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/cron", cron);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const data = require("../data.json");
+const update = require("../update.json");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/data", function (req, res, next) {
+  res.json(data);
+});
+
+router.get("/update", function (req, res, next) {
+  res.json(update);
 });
 
 module.exports = router;

bf1098ebcd5d74f2429bfad4a5623d964066c64e
LutfiArdiansyah <lutfi_ardiansyah@ymail.com>
2020-06-10 13:36:01 +0700

    Reschedule

--- cron.js
+++ cron.js
@@ -4,7 +4,7 @@ var schedule = require("node-schedule");
 var https = require("https");
 var fs = require("fs");
 
-schedule.scheduleJob("*/1 * * * *", async function () {
+schedule.scheduleJob("*/15 * * * *", async function () {
   let rmData = await new fs.unlinkSync("data.json");
   let data = await new fs.createWriteStream("data.json");
   https.get("https://data.covid19.go.id/public/api/data.json", function (

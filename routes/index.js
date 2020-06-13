97b39d710d8a0936fc6256eb63885cc5617ee15d
LutfiArdiansyah <lutfi_ardiansyah@ymail.com>
2020-06-13 12:59:12 +0700

    Renew

--- routes/index.js
+++ routes/index.js
@@ -10,44 +10,48 @@ router.get("/", function (req, res, next) {
 
 router.get("/data", function (req, res, next) {
   let body = [];
-  https.get("https://data.covid19.go.id/public/api/data.json", function (
-    response
-  ) {
-    response.on("data", (val) => {
-      body.push(val);
-    });
-    response.on("end", () =>
-      res.json(JSON.parse(Buffer.concat(body).toString()))
-    );
-  });
+  https.get(
+    "https://data.covid19.go.id/public/api/data.json_=" + Date.now().toString(),
+    function (response) {
+      response.on("data", (val) => {
+        body.push(val);
+      });
+      response.on("end", () =>
+        res.json(JSON.parse(Buffer.concat(body).toString()))
+      );
+    }
+  );
 });
 
 router.get("/update", function (req, res, next) {
   let body = [];
-  https.get("https://data.covid19.go.id/public/api/update.json", function (
-    response
-  ) {
-    response.on("data", (val) => {
-      body.push(val);
-    });
-    response.on("end", () =>
-      res.json(JSON.parse(Buffer.concat(body).toString()))
-    );
-  });
+  https.get(
+    "https://data.covid19.go.id/public/api/update.json_=" +
+      Date.now().toString(),
+    function (response) {
+      response.on("data", (val) => {
+        body.push(val);
+      });
+      response.on("end", () =>
+        res.json(JSON.parse(Buffer.concat(body).toString()))
+      );
+    }
+  );
 });
 
 router.get("/prov", function (req, res, next) {
   let body = [];
-  https.get("https://data.covid19.go.id/public/api/prov.json", function (
-    response
-  ) {
-    response.on("data", (val) => {
-      body.push(val);
-    });
-    response.on("end", () =>
-      res.json(JSON.parse(Buffer.concat(body).toString()))
-    );
-  });
+  https.get(
+    "https://data.covid19.go.id/public/api/prov.json_=" + Date.now().toString(),
+    function (response) {
+      response.on("data", (val) => {
+        body.push(val);
+      });
+      response.on("end", () =>
+        res.json(JSON.parse(Buffer.concat(body).toString()))
+      );
+    }
+  );
 });
 
 module.exports = router;

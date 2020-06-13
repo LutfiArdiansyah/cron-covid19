c149e6de3ace6e3856ea91ea4d77aa880fb35844
LutfiArdiansyah <lutfi_ardiansyah@ymail.com>
2020-06-13 12:56:19 +0700

    Renew

--- cron.js
+++ cron.js
@@ -9,29 +9,33 @@ schedule.scheduleJob("*/60 * * * *", async function () {
     let rmData = await new fs.unlinkSync("data.json");
   }
   let data = await new fs.createWriteStream("data.json");
-  https.get("https://data.covid19.go.id/public/api/data.json", function (
-    response
-  ) {
-    response.pipe(data);
-  });
+  https.get(
+    "https://data.covid19.go.id/public/api/data.json_=" + Date.now().toString(),
+    function (response) {
+      response.pipe(data);
+    }
+  );
   if (fs.existsSync("update.json")) {
     let rmUpdate = await new fs.unlinkSync("update.json");
   }
   let update = await new fs.createWriteStream("update.json");
-  https.get("https://data.covid19.go.id/public/api/update.json", function (
-    response
-  ) {
-    response.pipe(update);
-  });
+  https.get(
+    "https://data.covid19.go.id/public/api/update.json?_=" +
+      Date.now().toString(),
+    function (response) {
+      response.pipe(update);
+    }
+  );
 
   if (fs.existsSync("prov.json")) {
     let rmProv = await new fs.unlinkSync("prov.json");
   }
   let prov = await new fs.createWriteStream("prov.json");
-  https.get("https://data.covid19.go.id/public/api/prov.json", function (
-    response
-  ) {
-    response.pipe(prov);
-  });
+  https.get(
+    "https://data.covid19.go.id/public/api/prov.json_=" + Date.now().toString(),
+    function (response) {
+      response.pipe(prov);
+    }
+  );
 });
 module.exports = router;
